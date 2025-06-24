ab6287e4c8172d442839943fab4c475800ea4984
Bertrand <bertrand.tang.dev@gmail.com>
2025-06-24 16:43:10 +0200

    uses react hook form

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,99 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container"; 
+import Container from "react-bootstrap/Container";
 
 function App() {
-  const [formData, setFormData] = useState({
-    name: "",
-    date: "",
-    priority: "1",
-    isCompleted: false,
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      date: "",
+      priority: "1",
+      isCompleted: false,
+    },
   });
 
-  const handleChange = (event) => {
-    const {name, value, type, checked} = event.target;
-    setFormData((prevData) => ({
-      ...prevData, // On décompose le formulaire existant pour garder toutes les valeurs
-      [name]: type === "checkbox" ? checked : value, // On met à jour le champ en question grâce aux attributs name et value
-    }));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(formData);
+  const onSubmit = (data) => {
+    console.log("Données du formulaire soumises (valides) :", data);
+    reset;
   };
 
   return (
-     <Container className="d-flex justify-content-center align-items-center vh-100">
-    <Form onSubmit={handleSubmit}>
-      <Row>
-        <Form.Group controlId="name_input">
-          <Form.Label>Nom</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            name="name"
-            value={formData.name}
-            placeholder="Nom"
-            onChange={handleChange}
-          />
-        </Form.Group>
-        <Form.Group controlId="date_input">
-          <Form.Label>Date</Form.Label>
-          <Form.Control
-            required
-            type="date"
-            name="date"
-            value={formData.date}
-            onChange={handleChange}
-          />
-        </Form.Group>
-        <Form.Group controlId="select_input">
-          <Form.Label>Priorité</Form.Label>
-          <Form.Select
-            aria-label="select_priority"
-            name="priority"
-            value={formData.priority}
-            onChange={handleChange}
-          >
-            <option value="1">Basse</option>
-            <option value="2">Moyenne</option>
-            <option value="3">Haute</option>
-          </Form.Select>
-        </Form.Group>
-      </Row>
-      <Row>
-        <Form.Group controlId="checkbox_input">
-          <Form.Label>isCompleted</Form.Label>
-          <Form.Check
-            inline
-            name="isCompleted"
-            type="checkbox"
-            id="checkbox"
-            onChange={handleChange}
-            checked={formData.isChecked}
-          />
-        </Form.Group>
-      </Row>
-      <Button type="submit">Soumettre le formulaire</Button>
-    </Form>
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Row className="mb-3">
+          <Form.Group controlId="name_input">
+            <Form.Label>Nom</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Nom"
+              {...register("name", { required: "Le nom est requis." })}
+              isInvalid={!!errors.name} // convertit en booléen strict donc si errors.name existe alors ce serait true
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.name?.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row>
+          <Form.Group controlId="date_input">
+            <Form.Label>Date</Form.Label>
+            <Form.Control
+              type="date"
+              {...register("date", { required: "La date est requise." })}
+              isInvalid={!!errors.date}
+            />
+          <Form.Control.Feedback type="invalid">
+            {errors.date?.message}
+          </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row>
+          <Form.Group controlId="select_input">
+            <Form.Label>Priorité</Form.Label>
+            <Form.Select
+              aria-label="select_priority"
+              {...register("priority", {
+                required: "La priorité est requise.",
+              })}
+              isInvalid={!!errors.priority}
+            >
+              <option value="1">Basse</option>
+              <option value="2">Moyenne</option>
+              <option value="3">Haute</option>
+            </Form.Select>
+          <Form.Control.Feedback type="invalid">
+            {errors.priority?.message}
+          </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row>
+          <Form.Group controlId="checkbox_input">
+            <Form.Label>isCompleted</Form.Label>
+            <Form.Check
+              inline
+              type="checkbox"
+              id="isCompleted_checkbox"
+              {...register("isCompleted", {required: "."})}
+              isInvalid={!!errors.isCompleted}
+            />
+          <Form.Control.Feedback type="invalid">
+            {errors.isCompleted?.message}
+          </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <div>
+          <Button type="submit" variant="primary">
+            Soumettre le formulaire
+          </Button>
+        </div>
+      </Form>
     </Container>
   );
 }
