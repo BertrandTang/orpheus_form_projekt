c82855f84fa08e81c26b2535c63416e3d53f412d
Bertrand <bertrand.tang.dev@gmail.com>
2025-06-24 15:35:45 +0200

    change birthdate -> date, checkbox state handled in onchange, remove novalidate, id > controlId and minors changes.

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,21 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
+import Container from "react-bootstrap/Container"; 
 
 function App() {
   const [formData, setFormData] = useState({
     name: "",
-    birthdate: "",
-    priority: "",
+    date: "",
+    priority: "1",
     isCompleted: false,
   });
 
   const handleChange = (event) => {
+    const {name, value, type, checked} = event.target;
     setFormData((prevData) => ({
       ...prevData, // On décompose le formulaire existant pour garder toutes les valeurs
-      [event.target.name]: event.target.value, // On met à jour le champ en question grâce aux attributs name et value
+      [name]: type === "checkbox" ? checked : value, // On met à jour le champ en question grâce aux attributs name et value
     }));
   };
 
@@ -25,60 +26,60 @@ function App() {
   };
 
   return (
-    <Container>
-      <Form noValidate onSubmit={handleSubmit}>
-        <Row>
-          <Form.Group id="name_input">
-            <Form.Label>Nom</Form.Label>
-            <Form.Control
-              required
-              type="text"
-              name="name"
-              value={formData.name}
-              placeholder="Nom"
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group id="birthdate_input">
-            <Form.Label>Date de naissance</Form.Label>
-            <Form.Control
-              required
-              type="date"
-              name="birthdate"
-              value={formData.birthdate}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group id="select_input">
-            <Form.Label>Priorité</Form.Label>
-            <Form.Select
-              aria-label="select_priority"
-              name="priority"
-              value={formData.priority}
-              onChange={handleChange}
-            >
-              <option value="1">Basse</option>
-              <option value="2">Moyenne</option>
-              <option value="3">Haute</option>
-            </Form.Select>
-          </Form.Group>
-        </Row>
-        <Row>
-          <Form.Group id="checkbox_input">
-            <Form.Label>isCompleted</Form.Label>
-            <Form.Check
-              inline
-              label="isCompleted"
-              name="isCompleted"
-              type="checkbox"
-              id="checkbox"
-              onChange={handleChange}
-              checked={formData.isChecked}
-            />
-          </Form.Group>
-        </Row>
-        <Button type="submit">Soumettre le formulaire</Button>
-      </Form>
+     <Container className="d-flex justify-content-center align-items-center vh-100">
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Form.Group controlId="name_input">
+          <Form.Label>Nom</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            name="name"
+            value={formData.name}
+            placeholder="Nom"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="date_input">
+          <Form.Label>Date de naissance</Form.Label>
+          <Form.Control
+            required
+            type="date"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="select_input">
+          <Form.Label>Priorité</Form.Label>
+          <Form.Select
+            aria-label="select_priority"
+            name="priority"
+            value={formData.priority}
+            onChange={handleChange}
+          >
+            <option value="1">Basse</option>
+            <option value="2">Moyenne</option>
+            <option value="3">Haute</option>
+          </Form.Select>
+        </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group controlId="checkbox_input">
+          <Form.Label>isCompleted</Form.Label>
+          <Form.Check
+            inline
+            label="isCompleted"
+            name="isCompleted"
+            type="checkbox"
+            id="checkbox"
+            onChange={handleChange}
+            checked={formData.isChecked}
+          />
+        </Form.Group>
+      </Row>
+      <Button type="submit">Soumettre le formulaire</Button>
+    </Form>
     </Container>
   );
 }
